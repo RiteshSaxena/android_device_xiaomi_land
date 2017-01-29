afba2196827a358d41b65a392158c07484695fcc
Alberto97 <albertop2197@gmail.com>
2017-01-29 07:53:16 +0100

    volte: Add IMS shim

--- libshims/Android.mk
+++ libshims/Android.mk
@@ -65,5 +65,14 @@ LOCAL_C_INCLUDES += \
 
 LOCAL_MODULE:= libshim_ims_camera
 LOCAL_MODULE_TAGS := optional
-LOCAL_32_BIT_ONLY := true
+
+include $(BUILD_SHARED_LIBRARY)
+
+include $(CLEAR_VARS)
+
+LOCAL_SRC_FILES := ims/MediaBuffer.c
+LOCAL_SHARED_LIBRARIES := libstagefright_foundation
+LOCAL_MODULE := libshim_ims
+LOCAL_MODULE_TAGS := optional
+
 include $(BUILD_SHARED_LIBRARY)
--- product/camera.mk
+++ product/camera.mk
@@ -5,8 +5,12 @@ PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.camera.full.xml:system/etc/permissions/android.hardware.camera.full.xml \
     frameworks/native/data/etc/android.hardware.camera.raw.xml:system/etc/permissions/android.hardware.camera.raw.xml
 
-# Snap
+# Libshims
 PRODUCT_PACKAGES += \
     libshims_camera \
     libshim_ims_camera \
+    libshim_ims
+
+# Snap
+PRODUCT_PACKAGES += \
     Snap
