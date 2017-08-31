3860665f6c2436274a8ac3cb0d98bbbe8ecbdeff
Hriday Sharma <hridaysharma42@gmail.com>
2017-08-31 23:53:39 +0000

    land: Use prebuilt consumerir
    * Also fix

--- product/consumerir.mk
+++ product/consumerir.mk
@@ -1,7 +1,3 @@
-# Consumerir
-PRODUCT_PACKAGES += \
-    consumerir.msm8937
-
 # Permissions
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.consumerir.xml:system/etc/permissions/android.hardware.consumerir.xml
