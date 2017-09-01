7654c24bdd4e071ba9ef9925bed3a20b238c0f01
Hriday Sharma <hridaysharma42@gmail.com>
2017-09-01 01:00:04 +0000

    land: Update setup-makefiles.sh

--- setup-makefiles.sh
+++ setup-makefiles.sh
@@ -48,9 +48,6 @@ write_makefiles "$MY_DIR"/proprietary-files.txt
 
 cat << EOF >> "$ANDROIDMK"
 
-\$(shell mkdir -p \$(PRODUCT_OUT)/system/vendor/lib/egl && pushd \$(PRODUCT_OUT)/system/vendor/lib > /dev/null && ln -s egl/libEGL_adreno.so libEGL_adreno.so && popd > /dev/null)
-\$(shell mkdir -p \$(PRODUCT_OUT)/system/vendor/lib64/egl && pushd \$(PRODUCT_OUT)/system/vendor/lib64 > /dev/null && ln -s egl/libEGL_adreno.so libEGL_adreno.so && popd > /dev/null)
-
 EOF
 
 # Append the calls to firmware images
