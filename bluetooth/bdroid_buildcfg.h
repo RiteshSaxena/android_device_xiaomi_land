98077b024fec2a2f27baff2c9f1f48b240e709f9
Hriday Sharma <hridaysharma42@gmail.com>
2017-09-01 01:09:51 +0000

    land: Update bdroid_buildcfg from caf

--- bluetooth/bdroid_buildcfg.h
+++ bluetooth/bdroid_buildcfg.h
@@ -1,6 +1,5 @@
-/**
- * Copyright (C) 2012 The Android Open Source Project
- * Copyright (C) 2016 The CyanogenMod Project
+/*
+ * Copyright (C) 2017 The LineageOS Project
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -17,9 +16,18 @@
 
 #ifndef _BDROID_BUILDCFG_H
 #define _BDROID_BUILDCFG_H
-
-#define BTM_DEF_LOCAL_NAME   "Redmi"
-
+#define BTM_DEF_LOCAL_NAME   "Xiaomi Redmi 3S"
+// Disables read remote device feature
+#define BTA_SKIP_BLE_READ_REMOTE_FEAT FALSE
+#define MAX_ACL_CONNECTIONS    7
+#define MAX_L2CAP_CHANNELS    16
 #define BLE_VND_INCLUDED   TRUE
+// skips conn update at conn completion
+#define BTA_BLE_SKIP_CONN_UPD  FALSE
+#define BLE_PERIPHERAL_ADV_NAME  FALSE
 #define BT_CLEAN_TURN_ON_DISABLED 1
+
+#define BTA_DISABLE_DELAY 1000 /* in milliseconds */
+#define BTM_WBS_INCLUDED TRUE
+#define BTIF_HF_WBS_PREFERRED TRUE
 #endif
