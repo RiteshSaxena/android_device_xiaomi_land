84314fd1719e5f640eef1704c3d70e5006adc003
Hriday Sharma <hridaysharma42@gmail.com>
2017-09-01 13:06:53 +0000

    land: Fix Makefile name

--- product/display.mk
+++ product/display.mk
@@ -20,6 +20,7 @@ PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.opengles.aep.xml:system/etc/permissions/android.hardware.opengles.aep.xml
 
 # Vulkan
+PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.vulkan.level-0.xml:system/etc/permissions/android.hardware.vulkan.level-0.xml \
     frameworks/native/data/etc/android.hardware.vulkan.version-1_0_3.xml:system/etc/permissions/android.hardware.vulkan.version-1_0_3.xml
 
