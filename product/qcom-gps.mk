e9350a33f67b4c144cfdc266dfcd25f48e8edde9
Hriday Sharma <hridaysharma42@gmail.com>
2017-09-01 01:07:56 +0000

    land: Update GPS HAL to LA.UM.5.6.r1-04700-89xx.0

--- product/qcom-gps.mk
+++
@@ -1,9 +0,0 @@
-# GPS
-PRODUCT_BOOT_JARS += \
-    com.qti.location.sdk
-
-# Properties
-PRODUCT_PROPERTY_OVERRIDES += \
-    persist.gps.qc_nlp_in_use=1 \
-    persist.loc.nlp_name=com.qualcomm.location \
-    ro.gps.agps_provider=1
