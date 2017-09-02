67d7d3bdfc1e57d8085a766552d76ac66d49a691
Hriday Sharma <hridaysharma42@gmail.com>
2017-09-02 01:43:58 +0000

    land: Complete https://github.com/HridayHS/android_device_xiaomi_land/commit/dd607786e57ec37e62daa6a845347e086cd131b8

--- product/qcom-fm.mk
+++ product/qcom-fm.mk
@@ -1,13 +1,7 @@
 # FM
 PRODUCT_PACKAGES += \
-    FM2 \
-    libfmjni \
-    libqcomfm_jni \
-    qcom.fmradio
-
-# Init
-PRODUCT_PACKAGES += \
-    init.qcom.fm.sh
+    FMRadio \
+    libfmjni
 
 # Properties
 PRODUCT_PROPERTY_OVERRIDES += \
