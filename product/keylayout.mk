668fc5125d3de9be9e738a2cb69e4ec4725315e0
Hriday Sharma <hridaysharma42@gmail.com>
2017-09-27 11:34:09 +0000

    land: Add keylayouts
    * Match keylayout according to our display panel

---
+++ product/keylayout.mk
@@ -0,0 +1,6 @@
+# Keylayout
+PRODUCT_COPY_FILES += \
+    $(DEVICE_PATH)/keylayout/ft5x06_720p.kl:system/usr/keylayout/ft5x06_720p.kl \
+    $(DEVICE_PATH)/keylayout/gpio-keys.kl:system/usr/keylayout/gpio-keys.kl \
+    $(DEVICE_PATH)/keylayout/synaptics_dsx.kl:system/usr/keylayout/synaptics_dsx.kl \
+    $(DEVICE_PATH)/keylayout/synaptics_rmi4_i2c.kl:system/usr/keylayout/synaptics_rmi4_i2c.kl
