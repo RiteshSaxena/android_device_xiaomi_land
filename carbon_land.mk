2535076382c41de573aac5c6366a610b0785a2db
Ritesh Saxena <riteshsax007@gmail.com>
2017-10-04 15:30:25 +0000

    land: Add maintainer name

--- carbon_land.mk
+++ carbon_land.mk
@@ -41,6 +41,9 @@ PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
 
 TARGET_VENDOR_PRODUCT_NAME := land
 
+PRODUCT_PROPERTY_OVERRIDES += \
+    ro.carbon.maintainer="RiteshSaxena"
+
 PRODUCT_BUILD_PROP_OVERRIDES += \
     BUILD_FINGERPRINT=Xiaomi/land/land:6.0.1/MMB29M/V8.2.5.0.MALMIDL:user/release-keys \
     PRIVATE_BUILD_DESC="land-user 6.0.1 MMB29M V8.2.5.0.MALMIDL release-keys"
