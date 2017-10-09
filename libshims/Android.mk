yu-community-os/android_vendor_volte

volte: Shim old camera client to ims-camera

libshims/Android.mk
@@ -39,3 +39,44 @@ LOCAL_MODULE_CLASS := SHARED_LIBRARIES
 
 include $(BUILD_SHARED_LIBRARY)
 
+include $(CLEAR_VARS)
+
+LOCAL_SRC_FILES:= \
+	camera/Camera.cpp \
+	camera/CameraMetadata.cpp \
+	camera/CaptureResult.cpp \
+	camera/CameraParameters2.cpp \
+	camera/ICamera.cpp \
+	camera/ICameraClient.cpp \
+	camera/ICameraService.cpp \
+	camera/ICameraServiceListener.cpp \
+	camera/ICameraServiceProxy.cpp \
+	camera/ICameraRecordingProxy.cpp \
+	camera/ICameraRecordingProxyListener.cpp \
+	camera/camera2/ICameraDeviceUser.cpp \
+	camera/camera2/ICameraDeviceCallbacks.cpp \
+	camera/camera2/CaptureRequest.cpp \
+	camera/camera2/OutputConfiguration.cpp \
+	camera/CameraBase.cpp \
+	camera/CameraUtils.cpp \
+	camera/VendorTagDescriptor.cpp \
+	camera/CameraParameters.cpp
+
+LOCAL_SHARED_LIBRARIES := \
+	libcutils \
+	libutils \
+	liblog \
+	libbinder \
+	libhardware \
+	libui \
+	libgui \
+	libcamera_metadata
+
+LOCAL_C_INCLUDES += \
+	$(LOCAL_PATH)/camera/include \
+	system/media/camera/include \
+	system/media/private/camera/include
+
+LOCAL_MODULE:= libshims_camera
+
+include $(BUILD_SHARED_LIBRARY)

RiteshSaxena/android_device_xiaomi_land

libshims/Android.mk
@@ -25,3 +25,46 @@ LOCAL_MODULE := libshims_camera
 LOCAL_MODULE_TAGS := optional
 LOCAL_32_BIT_ONLY := true
 include $(BUILD_SHARED_LIBRARY)
+
+include $(CLEAR_VARS)
+
+LOCAL_SRC_FILES:= \
+	camera/Camera.cpp \
+	camera/CameraMetadata.cpp \
+	camera/CaptureResult.cpp \
+	camera/CameraParameters2.cpp \
+	camera/ICamera.cpp \
+	camera/ICameraClient.cpp \
+	camera/ICameraService.cpp \
+	camera/ICameraServiceListener.cpp \
+	camera/ICameraServiceProxy.cpp \
+	camera/ICameraRecordingProxy.cpp \
+	camera/ICameraRecordingProxyListener.cpp \
+	camera/camera2/ICameraDeviceUser.cpp \
+	camera/camera2/ICameraDeviceCallbacks.cpp \
+	camera/camera2/CaptureRequest.cpp \
+	camera/camera2/OutputConfiguration.cpp \
+	camera/CameraBase.cpp \
+	camera/CameraUtils.cpp \
+	camera/VendorTagDescriptor.cpp \
+	camera/CameraParameters.cpp
+
+LOCAL_SHARED_LIBRARIES := \
+	libcutils \
+	libutils \
+	liblog \
+	libbinder \
+	libhardware \
+	libui \
+	libgui \
+	libcamera_metadata
+
+LOCAL_C_INCLUDES += \
+	$(LOCAL_PATH)/camera/include \
+	system/media/camera/include \
+	system/media/private/camera/include
+
+LOCAL_MODULE:= libshim_ims_camera
+LOCAL_MODULE_TAGS := optional
+LOCAL_32_BIT_ONLY := true
+include $(BUILD_SHARED_LIBRARY)