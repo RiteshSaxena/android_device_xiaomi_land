c40517a81d5edab11dcc44866cbba9dc54bcedb8
Ritesh Saxena <riteshsax007@gmail.com>
2017-11-14 15:10:46 +0000

    land: update bt config

--- bluetooth/bdroid_buildcfg.h
+++ bluetooth/bdroid_buildcfg.h
@@ -16,18 +16,27 @@
 
 #ifndef _BDROID_BUILDCFG_H
 #define _BDROID_BUILDCFG_H
-#define BTM_DEF_LOCAL_NAME   "Xiaomi Redmi 3S"
-// Disables read remote device feature
-#define BTA_SKIP_BLE_READ_REMOTE_FEAT FALSE
-#define MAX_ACL_CONNECTIONS    7
+
+#include <cutils/properties.h>
+#include <string.h>
+
+inline const char* BtmGetDefaultName()
+{
+	char board_id[PROPERTY_VALUE_MAX];
+	property_get("ro.product.wt.boardid", board_id, "");
+
+	if (!strcmp("S88537AB1", board_id)) {
+		return "Redmi 3X";
+	}
+
+	return "Redmi 3S";
+}
+
+#define BTM_DEF_LOCAL_NAME BtmGetDefaultName()
+
+#define MAX_ACL_CONNECTIONS   16
 #define MAX_L2CAP_CHANNELS    16
 #define BLE_VND_INCLUDED   TRUE
-// skips conn update at conn completion
-#define BTA_BLE_SKIP_CONN_UPD  FALSE
-#define BLE_PERIPHERAL_ADV_NAME  FALSE
 #define BT_CLEAN_TURN_ON_DISABLED 1
 
-#define BTA_DISABLE_DELAY 1000 /* in milliseconds */
-#define BTM_WBS_INCLUDED TRUE
-#define BTIF_HF_WBS_PREFERRED TRUE
 #endif
