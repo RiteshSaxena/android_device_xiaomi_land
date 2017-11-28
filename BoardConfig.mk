592819ab228e33e968e8a5f59d61706135ed423f
Ritesh Saxena <riteshsax007@gmail.com>
2017-11-28 12:10:32 +0000

    land: Remove deprecated TARGET_KERNEL_APPEND_DTB flag

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -45,8 +45,6 @@ BOARD_KERNEL_IMAGE_NAME := Image.gz-dtb
 BOARD_KERNEL_PAGESIZE :=  2048
 BOARD_MKBOOTIMG_ARGS := --ramdisk_offset 0x01000000 --second_offset 0x00f00000 --tags_offset 0x00000100
 
-TARGET_KERNEL_APPEND_DTB := true
-
 TARGET_KERNEL_CONFIG := lineageos_land_defconfig
 TARGET_KERNEL_SOURCE := kernel/xiaomi/msm8937
 
