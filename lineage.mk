5edd6b3ab3e562e94761692af0f84c3d85965d75
Bruno Martins <bgcngm@gmail.com>
2017-12-01 21:49:26 +0000

    land: Apply LineageOS rebrand

--- lineage.mk
+++ lineage.mk
@@ -22,7 +22,7 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
 # Inherit some common Lineage stuff
-$(call inherit-product, vendor/cm/config/common_full_phone.mk)
+$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
 
 # Inherit from land device
 $(call inherit-product, $(DEVICE_PATH)/device.mk)
