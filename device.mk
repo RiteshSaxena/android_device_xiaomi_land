dcd6c15e1da302a5c7152de2ded8cb6ce98bc9aa
Ritesh Saxena <riteshsax007@gmail.com>
2017-12-23 13:49:51 +0000

    land: update wifi stuff from tissot
    Change-Id: Ib27ba81f31b25fb18585b4a0c75afb3939d3b16e

--- device.mk
+++ device.mk
@@ -379,4 +379,4 @@ PRODUCT_COPY_FILES += \
 
 PRODUCT_COPY_FILES += \
     $(DEVICE_PATH)/wifi/WCNSS_cfg.dat:system/etc/firmware/wlan/prima/WCNSS_cfg.dat \
-    $(DEVICE_PATH)/wifi/WCNSS_qcom_cfg.ini:system/etc/wifi/WCNSS_qcom_cfg.ini
+    $(DEVICE_PATH)/wifi/WCNSS_qcom_cfg.ini:system/vendor/etc/wifi/WCNSS_qcom_cfg.ini
