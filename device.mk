6eb0554cbe156ff83fab90b21040218b12339d48
Hriday Sharma <hridaysharma42@gmail.com>
2018-01-06 14:30:57 +0000

    land: Update seccomp policy
    Change-Id: I5be994e8a6ed3d1a98043710e270a82e25511b41

--- device.mk
+++ device.mk
@@ -348,7 +348,8 @@ PRODUCT_COPY_FILES += \
 
 # Seccomp policy
 PRODUCT_COPY_FILES += \
-    $(DEVICE_PATH)/seccomp_policy/mediacodec.policy:system/vendor/etc/seccomp_policy/mediacodec.policy
+    $(DEVICE_PATH)/seccomp_policy/mediacodec.policy:system/vendor/etc/seccomp_policy/mediacodec.policy \
+    $(DEVICE_PATH)/seccomp_policy/mediaextractor.policy:system/vendor/etc/seccomp_policy/mediaextractor.policy
 
 # Snap
 PRODUCT_PACKAGES += \
