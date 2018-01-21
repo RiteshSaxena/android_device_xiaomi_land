d59f53c40cf4ef21402474d4de01210d6612b44f
Bruno Martins <bgcngm@gmail.com>
2018-01-21 09:01:19 +0000

    land: Convert audio_effects.conf to XML
     * Based on https://android.googlesource.com/device/google/taimen/+/ecf82951a431324bd129b31495043dcfa00cfb1f
    
     * Global processor configurations discarded (useless anyway because
       those were related with Dirac effects applied to MUIU shipping apps).
    
     * Additionally to the same tests done by Google, it was verified that
       with this change "XML effect configuration loaded successfully.",
       while before it showed: "XML effect configuration failed to load.".
    
    Change-Id: I3fb868795d3df49a63839f29f8c42dba807943dc

--- device.mk
+++ device.mk
@@ -84,7 +84,7 @@ PRODUCT_PACKAGES += \
 PRODUCT_COPY_FILES += \
     $(DEVICE_PATH)/audio/audio_policy.conf:system/vendor/etc/audio_policy.conf \
     $(DEVICE_PATH)/audio/audio_output_policy.conf:system/vendor/etc/audio_output_policy.conf \
-    $(DEVICE_PATH)/audio/audio_effects.conf:system/vendor/etc/audio_effects.conf \
+    $(DEVICE_PATH)/audio/audio_effects.xml:system/vendor/etc/audio_effects.xml \
     $(DEVICE_PATH)/audio/mixer_paths_qrd_sku1.xml:system/vendor/etc/mixer_paths_qrd_sku1.xml \
     $(DEVICE_PATH)/audio/aanc_tuning_mixer.txt:system/vendor/etc/aanc_tuning_mixer.txt \
     $(DEVICE_PATH)/audio/audio_platform_info.xml:system/vendor/etc/audio_platform_info.xml \
