e09477b845f9f631c09c1bc3d8fa1a537dbe24e5
Ritesh Saxena <riteshsax007@gmail.com>
2018-01-21 10:43:19 +0000

    land: dexpreopt anyway
    Change-Id: I0e5e8abed1f5daee454cad0515fd894057eb71f4

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -128,9 +128,7 @@ ENABLE_CPUSETS := true
 # Dexpreopt
 ifeq ($(HOST_OS),linux)
   ifneq ($(TARGET_BUILD_VARIANT),eng)
-    ifeq ($(WITH_DEXPREOPT),)
-      WITH_DEXPREOPT := true
-    endif
+    WITH_DEXPREOPT := true
   endif
 endif
 WITH_DEXPREOPT_BOOT_IMG_AND_SYSTEM_SERVER_ONLY ?= true
