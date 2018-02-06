c239cfe7206847915ea990db7a64fd9a48640a28
Ritesh Saxena <riteshsax007@gmail.com>
2018-02-06 13:10:01 +0000

    land: Use thermal-engine config from stock
    Change-Id: Id28a8ef8c079e31de87b8103881af9a09e52830d

--- device.mk
+++ device.mk
@@ -378,6 +378,9 @@ PRODUCT_PACKAGES += \
     android.hardware.thermal@1.0-impl \
     android.hardware.thermal@1.0-service
 
+PRODUCT_COPY_FILES += \
+    $(DEVICE_PATH)/configs/thermal-engine.conf:system/vendor/etc/thermal-engine.conf
+    
 # USB HAL
 PRODUCT_PACKAGES += \
     android.hardware.usb@1.0-service
