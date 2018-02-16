4b9a8f8c479f0c36f33aae6c27f80a8e5845c3d9
Bruno Martins <bgcngm@gmail.com>
2018-02-16 21:08:00 +0000

    land: Switch to vendor.display.config 1.1
     * No need to explicitly build the packages anymore,
       since they're now display HAL dependencies.
    
    Change-Id: Ied3be4c1e5fc7e01e0426268fb219c61a4be86e9

--- device.mk
+++ device.mk
@@ -153,9 +153,7 @@ PRODUCT_PACKAGES += \
     android.hardware.graphics.composer@2.1-service \
     android.hardware.memtrack@1.0-impl \
     android.hardware.memtrack@1.0-service \
-    android.hardware.configstore@1.0-service \
-    vendor.display.color@1.0-service \
-    vendor.display.color@1.0-impl
+    android.hardware.configstore@1.0-service
 
 # DRM
 PRODUCT_PACKAGES += \
