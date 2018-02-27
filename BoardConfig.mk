acae3f5b3177e4a9c2bc55ebbca07acfe8476748
Michael Bestas <mkbestas@lineageos.org>
2018-02-27 20:00:45 +0000

    land: Get rid of recovery-only fstab
     * Mounting /system and /vendor partitions is handled in kernel now,
       however removing the entries from fstab caused issues building
       the OTA. The workaround was to have a separate fstab, but turns out
       that simply setting the recoveryonly flag does the trick because
       those are then ignored during a normal Android boot.
    
    Change-Id: I2944384d0a1c41bc9f9f51e2e29daff2bed0a0f4

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -198,7 +198,7 @@ BOARD_USES_QCOM_HARDWARE := true
 BOARD_USES_QC_TIME_SERVICES := true
 
 # Recovery
-TARGET_RECOVERY_FSTAB := $(DEVICE_PATH)/rootdir/recovery/fstab.qcom
+TARGET_RECOVERY_FSTAB := $(DEVICE_PATH)/rootdir/etc/fstab.qcom
 
 # RIL
 PROTOBUF_SUPPORTED := true
