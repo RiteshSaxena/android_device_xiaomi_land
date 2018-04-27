e4e50cdbcad28385685e7b78e814379192c481ed
dianlujitao <dianlujitao@lineageos.org>
2018-04-27 17:47:00 +0000

    land: Properly copy vulkan permission xml
    Change-Id: I77ab52307fe12372744b399ac105c2aad2c056d4

--- device.mk
+++ device.mk
@@ -394,7 +394,7 @@ PRODUCT_PACKAGES += \
 
 # Vulkan
 PRODUCT_COPY_FILES += \
-    frameworks/native/data/etc/android.hardware.vulkan.compute-0.xml:system/etc/permissions/android.hardware.vulkan.compute.xml \
+    frameworks/native/data/etc/android.hardware.vulkan.compute-0.xml:system/etc/permissions/android.hardware.vulkan.compute-0.xml \
     frameworks/native/data/etc/android.hardware.vulkan.level-0.xml:system/etc/permissions/android.hardware.vulkan.level-0.xml \
     frameworks/native/data/etc/android.hardware.vulkan.version-1_0_3.xml:system/etc/permissions/android.hardware.vulkan.version-1_0_3.xml
 
