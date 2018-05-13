00c8e542792d41a41a0cccfd7f677c8c0acb5254
Ritesh Saxena <riteshsax007@gmail.com>
2018-05-13 10:35:20 +0000

    land: Update build fingerprint
    Change-Id: I7c5b240b4dd5c4c4aa9c481b19405a63c8ad5d83

--- lineage.mk
+++ lineage.mk
@@ -37,10 +37,10 @@ PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
 TARGET_VENDOR_PRODUCT_NAME := land
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
-    PRIVATE_BUILD_DESC="land-user 6.0.1 MMB29M V9.5.1.0.MALMIFA release-keys"
+    PRIVATE_BUILD_DESC="land-user 6.0.1 MMB29M V9.5.5.0.MALMIFA release-keys"
 
 # Set BUILD_FINGERPRINT variable
-BUILD_FINGERPRINT := "Xiaomi/land/land:6.0.1/MMB29M/V9.5.1.0.MALMIFA:user/release-keys"
+BUILD_FINGERPRINT := "Xiaomi/land/land:6.0.1/MMB29M/V9.5.5.0.MALMIFA:user/release-keys"
 
 PRODUCT_SYSTEM_PROPERTY_BLACKLIST += \
     ro.product.model
