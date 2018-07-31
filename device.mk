6f21b6764b0bd0ec89616a714f32e5b90d5c5364
Ritesh Saxena <riteshsax007@gmail.com>
2018-07-31 13:48:40 +0000

    Revert "land: Build QTI HIDL power 1.0 HAL"
    This reverts commit fc3b581a1fef3e046655211815cb4540c277d34e.

--- device.mk
+++ device.mk
@@ -315,7 +315,11 @@ PRODUCT_PACKAGES += \
 
 # Power
 PRODUCT_PACKAGES += \
-    android.hardware.power@1.0-service-qti
+    power.msm8937
+    
+PRODUCT_PACKAGES += \
+    android.hardware.power@1.0-service \
+    android.hardware.power@1.0-impl
 
 # Ramdisk
 PRODUCT_PACKAGES += \
