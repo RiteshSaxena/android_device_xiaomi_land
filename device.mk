5751cdf97b9a904a5bdd9d066de2662057416d3b
Ritesh Saxena <riteshsax007@gmail.com>
2018-08-07 01:32:48 +0000

    Revert "land: Build LiveDisplay HAL"
    This reverts commit 540905c0588faa9d77ff1c626d60e883cb451788.

--- device.mk
+++ device.mk
@@ -271,10 +271,6 @@ PRODUCT_PACKAGES += \
     android.hardware.light@2.0-impl \
     android.hardware.light@2.0-service
 
-# LiveDisplay native
-PRODUCT_PACKAGES += \
-    vendor.lineage.livedisplay@1.0-service-sdm
-
 # Media
 PRODUCT_PACKAGES += \
     libc2dcolorconvert \
