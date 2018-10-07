f14dff59b26e31bfab86d1533c552518b99d2a90
Ritesh Saxena <riteshsax007@gmail.com>
2018-10-07 06:18:23 +0000

    land: Drop libcameraservice shim
    Change-Id: I8c8332e8055cf33cc4aab7523f458785369de035

--- device.mk
+++ device.mk
@@ -129,13 +129,6 @@ PRODUCT_PACKAGES += \
     vendor.qti.hardware.camera.device@1.0 \
     vendor.qti.hardware.camera.device@1.0_vendor
 
-# Cameraservice shim
-PRODUCT_PACKAGES += \
-    libshim_cameraservice
-
-PRODUCT_COPY_FILES += \
-    $(DEVICE_PATH)/configs/cameraserver.rc:system/etc/init/cameraserver.rc \
-
 # Configstore
 PRODUCT_PACKAGES += \
     android.hardware.configstore@1.0-service
