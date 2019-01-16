8057cf0f3fe4f3af1bc140ec863d9cff4c6ebbba
dianlujitao <dianlujitao@lineageos.org>
2019-01-16 11:13:36 +0000

    land: update props for new BT HAL
    Change-Id: I9adeae4d8d390244edcec82f0ba6c99ae216a104

--- vendor_prop.mk
+++ vendor_prop.mk
@@ -48,7 +48,7 @@ PRODUCT_PROPERTY_OVERRIDES += \
 # Bluetooth
 PRODUCT_PROPERTY_OVERRIDES += \
     bluetooth.hfp.client=1 \
-    qcom.bluetooth.soc=smd \
+    vendor.qcom.bluetooth.soc=smd \
     ro.bluetooth.hfp.ver=1.7 \
     ro.qualcomm.bt.hci_transport=smd
 
