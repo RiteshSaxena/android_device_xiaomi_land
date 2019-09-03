bd74f31f78e2e925532ae0b8703eca91c62503e0
Erfan Abdi <erfangplus@gmail.com>
2019-09-04 03:20:57 +0430

    land: Force disable bluetooth audio hal 2.0
    Change-Id: I217becb041395865a1c6f997b0cbc0c994d04e78
    Signed-off-by: Erfan Abdi <erfangplus@gmail.com>

--- vendor_prop.mk
+++ vendor_prop.mk
@@ -52,7 +52,8 @@ PRODUCT_PROPERTY_OVERRIDES += \
     bluetooth.hfp.client=1 \
     vendor.qcom.bluetooth.soc=smd \
     ro.bluetooth.hfp.ver=1.7 \
-    ro.qualcomm.bt.hci_transport=smd
+    ro.qualcomm.bt.hci_transport=smd \
+    persist.bluetooth.bluetooth_audio_hal.disabled=true
 
 # Camera
 PRODUCT_PROPERTY_OVERRIDES += \
