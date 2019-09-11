0004aadb2c7b96ecc7fe9810418f073503d3d9f1
Ritesh Saxena <riteshsax007@gmail.com>
2019-09-11 18:16:57 +0000

    land: Inherit shipping api level via makefile
    Change-Id: I61c8184cc70242ba802355df6db7fb137b704c7d

--- reloaded_land.mk
+++ reloaded_land.mk
@@ -20,6 +20,7 @@ DEVICE_PATH := device/xiaomi/land
 # Inherit from those products. Most specific first.
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/product_launched_with_m.mk)
 
 # Inherit some common Reloaded-CAF stuff
 $(call inherit-product, vendor/reloaded/common.mk)
