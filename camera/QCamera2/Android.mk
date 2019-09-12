fe74240890f832a7641ca155b8b1864b4ffc6236
Ritesh Saxena <riteshsax007@gmail.com>
2019-09-12 04:56:23 +0000

    land: camera: Fix compile
    Change-Id: Iaf03af0de0ad5dbfd1c9964fd63ebf5cd70749ee

--- camera/QCamera2/Android.mk
+++ camera/QCamera2/Android.mk
@@ -39,7 +39,7 @@ LOCAL_SRC_FILES += \
         HAL/QCameraParametersIntf.cpp \
         HAL/QCameraThermalAdapter.cpp
 
-LOCAL_CFLAGS := -Wall -Wextra -Werror -Wno-unused-parameter -Wno-unused-variable
+LOCAL_CFLAGS := -Wall -Wextra -Werror -Wno-unused-parameter -Wno-unused-variable -Wno-implicit-fallthrough
 
 # System header file path prefix
 LOCAL_CFLAGS += -DSYSTEM_HEADER_PREFIX=sys
